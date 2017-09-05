cce0dd0d897bdd5de507d1c00679509eadc419ba
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-09-05 21:34:18 +0200

    Add GUI

---
+++ src/NewJFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class NewJFrame {
+    private JPanel panel;
+    private JButton insertButton;
+    private JButton deleteButton;
+    private JTextField textField1;
+}
